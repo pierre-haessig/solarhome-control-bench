7e02a0a03e8f4624ba734d4368e84e0716041d1e
pajjaecat <37332216+pajjaecat@users.noreply.github.com>
2018-06-29 12:56:38 +0200

    Change name to interleave_mat
    Change the name from reArrangeMat to interleave_mat

--- methods/matlab_mpc/reArrangeMat.m
+++ methods/matlab_mpc/reArrangeMat.m
@@ -1,7 +1,7 @@
-function out_data = reArrangeMat(varargin)
+function out_data = interleave_mat(varargin)
 %**********************************************************************
-%% HOME_SIM_MPC || Auxiliary function reArrangeMat
-% Version: 1.1
+%% HOME_SIM_MPC || Auxiliary function interleave_mat
+% Version: 1.2
 % Required functions: None
 % 
 % Inputs : 
@@ -34,19 +34,20 @@ function out_data = reArrangeMat(varargin)
 %**********************************************************************
 
  
-N_inp = nargin; % Get the number of input given to the function;
+N_inp = nargin - 1; % Get the number of input given to the function 
+% minus the first;
 horiz = varargin{1}; % Get the data within the first input which is the 
 %prediction horizon;
 
 cur_row = 1;% Current row. Variable Used to go from line 1 up to last line 
 % of every input
 
-for i=1:N_inp-1:horiz*(N_inp-1)
+for i=1:N_inp:horiz*N_inp
     
     cur_inp = 2; % Current input number % do not forget that we are using 
     %data from input_2 to input_N
     
-    for k=i:i+N_inp-2
+    for k=i:i+N_inp-1
         var1 = varargin{cur_inp};
         out_data(k,:) = var1(cur_row,:);
 
